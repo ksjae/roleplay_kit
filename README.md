26d68eb592b34b89417f9158ba9362596374b0ae
ksjae <ksjae0705@hotmail.com>
2023-04-09 09:51:56 +0100

    env file prep

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # roleplay_kit
 
 A new Flutter project for providing custom text game experience
-~~ and a Copilot playground ~~
+~~and a Copilot playground~~
 
 ## Features
 - AI-backed (thru API) content generation & presentation
 - Game Mechanincs running on top of ordinary LLMs
-- Non-text input support through callbacks?
+- Game can be influenced by non-text (but quantifiable) attr, through custom widgets
+
+Universal API mapping support is coming, but since OpenAI APIs are *reasonable* this project currently only supports chatGPT-backed content generation.
+
+## Running
+Add a `.env` file to project root.
+```
+// .env
+OPEN_AI_API_KEY=<REPLACE WITH YOUR API KEY>
+```
+Then, run `flutter pub run build_runner build` (one time only)
+
+### NOTE for non-alphabetic language
+* Depending on the Tokenizer, broken letters may appear. This WILL cause problems.
+* OpenAI's tokenizer is **VERY INEFFICIENT** at tokenizing Korean, resulting in expensive API costs(~3x from English).
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:dart_openai/openai.dart';
+import 'env/env.dart';
 
 void main() {
+  OpenAI.apiKey = Env.apiKey;
   runApp(const MainApp());
 }
 
@@ -10,11 +13,167 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: ChatWindow(),
+    );
+  }
+}
+
+class ChatWindow extends StatefulWidget {
+  const ChatWindow({super.key});
+
+  @override
+  State<ChatWindow> createState() => _ChatWindowState();
+}
+
+class _ChatWindowState extends State<ChatWindow> {
+  @override
+  Widget build(BuildContext context) {
+    var playerName = 'Mister Adventurer';
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        flexibleSpace: SafeArea(
+            child: Container(
+          padding: const EdgeInsets.only(right: 16),
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      playerName,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Online',
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Icon(
+                Icons.settings,
+                color: Colors.grey.shade700,
+              ),
+            ],
+          ),
+        )),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: 10,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return MessageBubble(
+                  message: "message", isUserInput: index % 2 == 0);
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Write message...',
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble(
+      {super.key, required this.message, required this.isUserInput});
+  final String message;
+  final bool isUserInput;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+      child: Align(
+        alignment: isUserInput ? Alignment.topLeft : Alignment.topRight,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: isUserInput ? Colors.grey.shade200 : Colors.blue[200],
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            message,
+            style: TextStyle(fontSize: 15),
+          ),
         ),
       ),
     );
+    ;
   }
 }
